9a620a26fe5fcc3d61a438a7f8a73ba2c0c877da
rayendito <ryanditod@gmail.com>
2021-07-17 14:19:09 +0700

    major correction kkwkw

--- game.py
+++ game.py
@@ -9,55 +9,64 @@ def printBoard(idx):
     print("┃ "+agentPos[0]+" ┃ "+agentPos[1]+" ┃ "+agentPos[2]+" ┃ "+agentPos[3]+" ┃ "+agentPos[4]+" ┃ "+agentPos[5]+" ┃ "+agentPos[6]+" ┃ "+agentPos[7]+" ┃ "+agentPos[8]+" ┃ "+agentPos[9]+" ┃")
     print("┗━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┛")
 
-def learn(qTable, currPos):
-    # update q value table
-    qTable[currPos][0] = qValue(qTable, currPos, -1)
-    qTable[currPos][0] = qValue(qTable, currPos, -1)
-    print(qTable)
+def learn(qTable, sqWeight, currPos, action):
+    # learning rate 1, berarti langsung update q value
+    qTable[currPos, action] = sqWeight[currPos+action]+max(qTable[currPos+action][0], qTable[currPos+action][1])
+    
 
-def qValue(qtable, state, action, discount):
-    if(state == 8 and action == 1): # nilai pisang
-        return 10
-    elif(state == 1 and action == -1): # nilai zonk
-        return -10
-    else:
-        return qtable[state, action] + discount*max(qtable[state+action, -1], qtable[state+action, 1])
-
-def bellmanEquation(lr, disc):
-    print("awokaowk")
-
-def main(epsilon):
-    # initialize current player location
+def main(learnSteps):
+    # initialize current player location, original learnSteps value
+    orLearn = learnSteps
     playerPosIdx = 2
+
+    # square weight
+    # mendekat hole dari posisi awal -1, mendekat banana 1
+    sqWeight = [-10, -1, 0, 1, 2, 3, 4, 5, 6, 10]
 
     # 2 x 10 soalnya 10 possible states and 2 possible moves
     # index 0 kiri, indeks 1 kanan
     qTable = [[0,0] for i in range(10)]
 
-    # update table and print out
-    learn(qTable, playerPosIdx)
-    printBoard(playerPosIdx)
+    # initialize epsilon to 1 awalnya, lama lama ngurang
+    epsilon = 1
 
-    while(0 < playerPosIdx and playerPosIdx < 9):
-        # explore or exploit berd random
+    #selama blm sampe goal
+    print("learning...")
+    while(0 < learnSteps):
+        # random dan inisialisasi epsilon
+        epsilon = 1
+        action = 0
         r = random()
-        if(r < epsilon): # random
+
+        # explore
+        if(r < epsilon):
             m = random()
             if(m <= 0.5):
-                playerPosIdx -= 1
+                action = -1
             else:
-                playerPosIdx += 1
-        else: # berd. tabel, q value yg paling tinggi
+                action = 1
+        # exploit
+        else:
             if(qTable[playerPosIdx][0] > qTable[playerPosIdx][1]):
-                playerPosIdx -= 1
+                action = -1
             else:
-                playerPosIdx += 1
+                action = 1
+        
+        # move and learn by ngupdate qtable nya
+        playerPosIdx += action
+        learn(qTable, sqWeight,  playerPosIdx, action)
+        
+        # printing hihi
+        print(qTable)
         printBoard(playerPosIdx)
-    
-    if(playerPosIdx == 9):
-        print("Menang!")
-    else:
-        print("Wait, this got printed?")
 
-# main()
-printBoard(3)
+        # updating values
+        epsilon -= 1/(orLearn//2)
+        learnSteps =- 1
+    
+    # if(playerPosIdx == 9):
+    #     print("Menang!")
+    # else:
+    #     print("Wait, this got printed?")
+
+main(12)
