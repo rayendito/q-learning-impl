e600d9f4408d7d0a86f00355974f0663f8570f36
rayendito <ryanditod@gmail.com>
2021-07-23 20:42:25 +0700

    qlearn done

--- game.py
+++ game.py
@@ -10,83 +10,78 @@ def printBoard(idx):
     print("┃ "+agentPos[0]+" ┃ "+agentPos[1]+" ┃ "+agentPos[2]+" ┃ "+agentPos[3]+" ┃ "+agentPos[4]+" ┃ "+agentPos[5]+" ┃ "+agentPos[6]+" ┃ "+agentPos[7]+" ┃ "+agentPos[8]+" ┃ "+agentPos[9]+" ┃")
     print("┗━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┛")
 
-def learn(qTable, sqWeight, currPos, action):
+def learn(qTable, rewards, lr, gamma, currPos, action):
     # learning rate 1, berarti langsung update q value
-    # ugly code ik :()
     if(action == 1):
         idex = 1
     else:
         idex = 0
     try:
-        qTable[currPos][idex] = sqWeight[currPos] + max(qTable[currPos+action][0], qTable[currPos+action][1])
+        qTable[currPos][idex] = qTable[currPos][idex] + lr*(rewards[currPos+action] + gamma*(max(qTable[currPos+action])) - qTable[currPos][idex])
     except:
         print(idex)
         print(currPos)
         print(action)
     
 
-def main(learnSteps):
-    # initialize current player location, original learnSteps value
-    orLearn = learnSteps
+def main():
+    # initializations
     playerPosIdx = 2
-
-    # square weight
-    # mendekat hole dari posisi awal -1, mendekat banana 1
-    sqWeight = [-10, -1, 0, 1, 2, 3, 4, 5, 6, 10]
-
-    # 2 x 10 soalnya 10 possible states and 2 possible moves
-    # index 0 kiri, indeks 1 kanan
+    sqWeight = [-15000, -1, 0, 1, 10, 100, 1000, 5000, 10000, 15000]
     qTable = [[0,0] for i in range(10)]
-
-    # initialize epsilon to 1 awalnya, lama lama ngurang
-    epsilon = 1
+    epsilon  = 0.2
+    score = 0
+    lr = 0.2
+    gamma = 0.8
 
     #selama blm sampe goal
     print("learning...")
-    while(0 < learnSteps):
-        # random dan inisialisasi epsilon
-        epsilon = 0.5
-        action = 0
+    while(-5 < score and score < 5):
+        # print current states to the screen
+        print("Current Q table :")
+        print(qTable)
+        printBoard(playerPosIdx)
+
+        # random to decide expolore apa exploit
         r = random()
 
         # explore
         if(r < epsilon):
+            print("Exploring!")
+            # random ke kanan apa ke kiri
             m = random()
+            # ke kiri
             if(m <= 0.5):
-                if(playerPosIdx == 0):
-                    action = 1
-                else:
-                    action = -1
+                action = -1
+            # ke kanan
             elif(0.5 < m):
-                if(playerPosIdx == 9):
-                    action = -1
-                else:
-                    action = 1
+                action = 1
         # exploit
         else:
-            if(qTable[playerPosIdx][0] > qTable[playerPosIdx][1]):
-                action = -1
-            else:
+            print("Exploiting!")
+            if(qTable[playerPosIdx][0] <= qTable[playerPosIdx][1]):
                 action = 1
+            else:
+                action = -1
         
-        # move and learn by ngupdate qtable nya
-        learn(qTable, sqWeight,  playerPosIdx, action)
+        #learn regardless
+        learn(qTable, sqWeight, lr, gamma, playerPosIdx, action)
         playerPosIdx += action
         
-        # printing hihi
-        # print(qTable)
-        # printBoard(playerPosIdx)
-        # time.sleep(1)
+        # check if at pojokan
+        if(playerPosIdx == 0 or playerPosIdx == 9):
+            if(playerPosIdx == 0):
+                score -= 1
+            else:
+                score += 1
+            playerPosIdx = 2
 
-        # updating values
-        epsilon -= 0.05
-        learnSteps -= 1
+        # sleep so we can see
+        time.sleep(1.5)
     
-    print(qTable)
-    print("I've learnt enough time to explore")
-    # if(playerPosIdx == 9):
-    #     print("Menang!")
-    # else:
-    #     print("Wait, this got printed?")
+    if(score == 5):
+        print("Menang! :D")
+    else:
+        print("Kalah.. :(")
 
-main(20)
+main()
